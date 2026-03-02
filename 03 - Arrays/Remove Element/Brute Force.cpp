f8df2fe4ef0e3c0e409e478d9c8b967aa24f2fd4
ascendho <coderhs@foxmail.com>
2026-03-02 10:57:25 +0800

    代码格式化

--- 03 - Arrays/Remove Element/Brute Force.cpp
+++ 03 - Arrays/Remove Element/Brute Force.cpp
@@ -1,7 +1,8 @@
 // 题目链接：https://leetcode.cn/problems/remove-element/
 
 // 暴力解法
-class Solution {
+class Solution
+{
 public:
     /**
      * @brief 暴力法原地移除数组中所有等于val的元素，并返回新数组的长度
@@ -9,22 +10,26 @@ public:
      * @param val 需要移除的目标值
      * @return int 移除元素后数组的新长度
      */
-    int removeElement(vector<int>& nums, int val) {
+    int removeElement(vector<int> &nums, int val)
+    {
         // 初始化数组的有效长度（初始为原始长度）
         int size = nums.size();
-        
+
         // 遍历数组，i为当前检查的元素下标
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++)
+        {
             // 发现当前元素是需要移除的目标值val
-            if (nums[i] == val) { 
+            if (nums[i] == val)
+            {
                 // 从i+1位置开始，将后续所有元素向前移动一位，覆盖当前的val
-                for (int j = i + 1; j < size; j++) {
+                for (int j = i + 1; j < size; j++)
+                {
                     nums[j - 1] = nums[j];
                 }
                 // 关键：元素集体前移后，原i+1位置的元素现在到了i位置，需要重新检查i
-                i--; 
+                i--;
                 // 数组有效长度减1（移除了一个val元素）
-                size--; 
+                size--;
             }
         }
         // 最终的有效长度即为移除val后的数组长度
--- 03 - Arrays/Remove Element/Two Pointers.cpp
+++ 03 - Arrays/Remove Element/Two Pointers.cpp
@@ -1,7 +1,8 @@
 // 题目链接：https://leetcode.cn/problems/remove-element/
 
 // 双指针解法
-class Solution {
+class Solution
+{
 public:
     /**
      * @brief 原地移除数组中所有等于val的元素，并返回新数组的长度
@@ -9,16 +10,19 @@ public:
      * @param val 需要移除的目标值
      * @return int 移除元素后数组的新长度，且数组前return个元素为非val值
      */
-    int removeElement(vector<int>& nums, int val) {
+    int removeElement(vector<int> &nums, int val)
+    {
         // 获取数组的原始长度
         int n = nums.size();
         // 左指针：指向新数组（非val元素）的待填充位置
         int left = 0;
-        
+
         // 右指针：遍历整个原始数组，寻找非val元素
-        for (int right = 0; right < n; right++) {
+        for (int right = 0; right < n; right++)
+        {
             // 当右指针找到非val元素时
-            if (nums[right] != val) {
+            if (nums[right] != val)
+            {
                 // 将该元素填充到左指针位置（覆盖原位置）
                 nums[left] = nums[right];
                 // 左指针右移，准备填充下一个非val元素
@@ -26,7 +30,7 @@ public:
             }
             // 若右指针找到val元素，直接跳过，左指针不移动
         }
-        
+
         // 左指针的最终位置即为新数组的长度（所有非val元素的个数）
         return left;
     }
